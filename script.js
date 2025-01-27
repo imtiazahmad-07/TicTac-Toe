ed2879d6b15eb6f9d2a9b627e27f07b124d172ad
Imtiaz-Ahmad <imtiazbaluc18@gmail.com>
2025-01-27 14:41:32 +0500

    added function to change the turn

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let audioTurn = new Audio('ting.mp3');
 let gameOver = new Audio('gameover.mp3');
 let isGameOver = false;
 let turn = "X";
+
+// Function to change the turn
+const changeTurn = () => {
+    music.play();
+    return turn === "X" ? "0" : "X";
+}
