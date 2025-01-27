14574b17da50c514955cbdceae6fb33869a8aac5
Imtiaz-Ahmad <imtiazbaluc18@gmail.com>
2025-01-27 14:42:34 +0500

    added function to check for winner

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ const changeTurn = () => {
     music.play();
     return turn === "X" ? "0" : "X";
 }
+
+// Function to check for a win
+const checkWin = () => {
+    let boxText = document.getElementsByClassName('boxText');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    wins.forEach(e => {
+        if (boxText[e[0]].innerText === boxText[e[1]].innerText && boxText[e[2]].innerText === boxText[e[0]].innerText && boxText[e[0]].innerText !== "") {
+            document.getElementsByClassName('info')[0].innerText = boxText[e[0]].innerText + " Won";
+            isGameOver = true;
+            document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "200px";
+            music.pause();
+            gameOver.play();
+        }
+    })
+}
